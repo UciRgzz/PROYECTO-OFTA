933cf523a61700e4b703b302b0a639f9a4fc62c5
Maro04 <1170266@alumno.um.edu.mx>
2025-08-21 19:40:14 -0600

    Conectar a PostgreSQL en Render con DATABASE_URL

--- server.js
+++ server.js
@@ -39,11 +39,8 @@ app.use(session({
     port: 5432
 });*/
 const pool = new Pool({
-  user: process.env.PGUSER,
-  host: process.env.PGHOST,
-  database: process.env.PGDATABASE,
-  password: process.env.PGPASSWORD,
-  port: process.env.PGPORT
+    connectionString: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false }
 });
 
 
