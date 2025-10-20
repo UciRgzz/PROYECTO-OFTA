036064e856e17dbaaa369400ec42c2af81c7a5c7
Maro04 <1170266@alumno.um.edu.mx>
2025-10-20 16:42:02 -0600

    Actualización de index

--- server.js
+++ server.js
@@ -2481,7 +2481,6 @@ app.get('/api/mis-permisos', verificarSesion, async (req, res) => {
 //===========MODULO DE FOTO EN EL USUARIO==================//
 // ==================== SUBIR FOTO DE PERFIL ====================
 const multerPerfil = require('multer');
-const fs = require('fs');
 
 const storagePerfil = multerPerfil.diskStorage({
   destination: function (req, file, cb) {
