c210088083d1ae6aec7be92088924961a660aa4e
Maro04 <1170266@alumno.um.edu.mx>
2025-10-21 10:10:26 -0600

    Actualización de index

--- server.js
+++ server.js
@@ -9,7 +9,6 @@ const crypto = require('crypto');
 const multer = require('multer');
 const xlsx = require('xlsx');
 const { deprecate } = require('util');
-const fs = require('fs');
 
 
 const app = express();
